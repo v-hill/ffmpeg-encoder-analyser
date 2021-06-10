1934a4c694a0669de534b0493b5cd1eaf48f7e44
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-06-10 21:01:49 +0100

    Update temp_video_clip.py

--- temp_video_clip.py
+++ temp_video_clip.py
@@ -1,17 +1,23 @@
-
 """
 Create a temporary video for analysis.
 """
 
 import os
-import os.path, time
+import os.path
+import time
 from glob import glob
 import datetime
 import cv2
 
-input_dir = ""
-input_video = ""
-output_dir = ""
+# -----------------------------------------------------------------------------
+
+def get_timestamp(input_name):
+    creation_time = time.ctime(os.path.getmtime(input_name))
+    creation_time = creation_time[4:]
+    new_creation = datetime.datetime.strptime(creation_time, '%b %d %H:%M:%S %Y')
+    new_creation = f"{new_creation:%Y-%m-%dT%H:%M:%S.000000Z}"
+    print(f"Old: {creation_time},    New: {new_creation}")
+    return new_creation
 
 def make_command(input_file, output_file, start, duration):
     command = f'ffmpeg -ss {int(start)} -i "{input_file}" '
@@ -19,11 +25,30 @@ def make_command(input_file, output_file, start, duration):
     command += f'"{output_file}"'
     return command
 
-start = 390    #seconds
+# -----------------------------------------------------------------------------
+
+input_dir = ""
+input_video = ""
+
+output_dir = ""
+
+start = 330    #seconds
 duration = 120    #seconds
+
+files = []
 
 input_name = input_dir + input_video
 output_video = input_name.split('\\')[-1].replace('.mp4', '_short.mp4')
+output_name = output_dir+output_video
+files.append(output_name)
 
-command = make_command(input_dir+input_video, output_dir+output_video, start, duration)
+command = make_command(input_dir+input_video, output_name, start, duration)
 print(command)
+
+os.system(command)
+stinfo = os.stat(input_name)
+os.utime(output_name,(stinfo.st_atime, stinfo.st_mtime))
+
+time.sleep(5)
+for f in files:
+    os.remove(f)
